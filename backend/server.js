7bd249680b20f1480446ed51e28e59c21a03f9c9
Vishnuraj-1012 <145826263+Vishnuraj-1012@users.noreply.github.com>
2026-05-15 12:47:32 +0530

    commiting cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin : "https://shadow-coach-peach.vercel.app",
+
+    }
+));
 app.use(express.json());
 
 let users = [];
